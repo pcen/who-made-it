0ac887beef892c2f8e49837436fd1c889037082d
Paul Cento <prscento@uwaterloo.ca>
2020-12-11 19:31:22 -0500

    adds router

---
+++ src/pages/AddIngredients.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddIngredients = (props) => {
+  return (
+    <React.Fragment>
+      <div>What ingredients do you have?</div>
+    </React.Fragment>
+  );
+}
+
+export default AddIngredients;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <React.Fragment>
+      <div>Who made it?</div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
