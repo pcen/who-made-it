cee6ae2c27f966a17238c58def2ee5d39d04b2f7
Shivani Chidella <shivani.chidella@gmail.com>
2020-12-12 00:44:04 -0500

    changes

--- src/pages/TheStory.js
+++ src/pages/TheStory.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import { generateStory } from '../logic/story-generation';
+import Recipes from '../Components/Recipes';
+
+import Elizabeth from '../assets/beth.png';
+import Margaret from '../assets/marge.png';
+import Raymond from '../assets/ray.png';
 
 import '../styles/the-story.css';
 
-const SampleRecipe = {
-  ingredients: [
-    {
-      ingredient: 'lemon',
-      amount: '5-6 dashes',
-    },
-    {
-      ingredient: 'scotch',
-      amount: '1 large glass',
-    },
-    {
-      ingredient: 'ice',
-      amount: '2-3 lumps',
-    },
-    {
-      ingredient: 'soda',
-      amount: '1 glass',
-    },
-  ],
-  steps: []
-};
+const ActorProfile = props => {
+  const { image, name, item } = props;
+  return (
+    <div className='actor-profile'>
+      <img className='actor-image' alt={name} src={image} />
+      <div className='actor-name'>{name}</div>
+      <div>{item}</div>
+    </div>
+  );
+}
 
 const TheStory = (props) => {
   const { recipe, options } = props;
-  let story = generateStory(SampleRecipe, ['Scotch Tom Collins', 'Scotch Rikey', 'Whiskey Sour']);
+  let story = generateStory(Recipes.drinks.ScotchTomCollins, ['Scotch Tom Collins', 'Scotch Rickey', 'Whiskey Sour']);
 
   return (
-    <React.Fragment>
-      <div className='story-container'>
-        <div className='story'>{story}</div>
+    <div className='story-container'>
+      <div className='story'>{story}</div>
+      <div className='actors-container'>
+        <ActorProfile name='Elizabeth' image={Elizabeth} item={'Scotch Tom Collins'} />
+        <ActorProfile name='Margaret' image={Margaret} item={'Scotch Rickey'} />
+        <ActorProfile name='Raymond' image={Raymond} item={'Whiskey Sour'} />
       </div>
-    </React.Fragment>
+    </div>
   );
 }
 
