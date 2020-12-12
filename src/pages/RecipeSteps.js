eaaac2e2eed8bc08a82e70987685084992d7f0de
Naomi <63750118+nvpaul@users.noreply.github.com>
2020-12-11 21:28:25 -0500

    working on recipe steps layout

---
+++ src/pages/RecipeSteps.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import "../styles/steps.css";
+
+import { Grid } from '@material-ui/core';
+
+
+//This page will provide the user with the steps to follow along with the poisoned recipe (minus the poison of coruse)
+//To help them figure out who the killer is, as soon as they find out what they are making
+const RecipeSteps = (props) => {
+  return (
+    <React.Fragment>
+      <div className="steps">
+        {/* The Title */}
+        <h1>Follow along with the recipe (minus the poison) to solve the mystery and find out who the killer is</h1>
+
+        <Grid container
+              direction="row"
+        >
+
+          <Grid container item xs={4}>
+            <div className="stepsPanel">
+              {/* The Recipe Steps */}
+              {/* Step 1 */}
+              <Grid item xs={12} className="stepNumber">
+                <div>Step 1</div>
+              </Grid>
+              <div>
+                {/* Ingredients required for step 1 */}
+                <div>Ingredients Needed</div>
+                <ul>
+                  <li>1 glass</li>
+                  <li>2 - 3 Lumps of Ice</li>
+                </ul>
+                <br></br>
+                {/* Instructions for step 1 */}
+                <div>Find a glass and add the ice</div>
+              </div>
+            </div>
+          </Grid>
+          
+          <Grid container item xs={8}>
+              <div className="stepsSuspectsMenu">
+                {/* The Suspects */}
+                Random text so I can see the background color :)           
+                {/* The Menu Options */}
+                Random text so I can see the background color :)
+              </div>
+          </Grid>
+          
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default RecipeSteps;
