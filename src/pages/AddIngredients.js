31ff25a9566c149b069af119ca809369c5671823
Marvi Naaz <marvinaaz@Marvis-MacBook-Air.local>
2020-12-12 10:12:20 -0500

    button

--- src/pages/AddIngredients.js
+++ src/pages/AddIngredients.js
@@ -8,6 +8,7 @@ import alcohol from "../assets/alcohol bottle.png";
 import soda from "../assets/soda.png";
 import sugar from "../assets/sugar.jpg";
 import recipes from "../Components/Recipes.js";
+import { Button } from "@material-ui/core";
 
 var ingredientArray = [];
 
@@ -15,64 +16,57 @@ const AddIngredients = (props) => {
   const onIngredientClick = (prop) => {
     console.log(ingredientArray.push(prop));
     console.log(ingredientArray);
-
   };
 
-
-  var potentialRecipies = []
+  var potentialRecipies = [];
 
   const Recipes = {
     drinks: {
       // Scotch Tom Collins
       ScotchTomCollins: {
-        ingredients: ['lemon', 'scotch', 'ice', 'soda'],
-        steps: [
-          'step 1',
-          'step 2',
-          'step 3',
-        ],
-        name: 'Scotch Tom Colins'
+        ingredients: ["lemon", "scotch", "ice", "soda"],
+        steps: ["step 1", "step 2", "step 3"],
+        name: "Scotch Tom Colins",
       },
       ScotchRickey: {
-        ingredients: ['lemon', 'scotch', 'ice', 'soda', 'lime'],
-        steps: [
-          'step 1',
-          'step 2',
-          'step 3',
-        ],
-        name: 'Scotch Rickey'
+        ingredients: ["lemon", "scotch", "ice", "soda", "lime"],
+        steps: ["step 1", "step 2", "step 3"],
+        name: "Scotch Rickey",
       },
-    }
-  }
+    },
+  };
 
   const recipeGenerator = () => {
-    console.log("text")
+    console.log("text");
 
     for (var i in Recipes.drinks) {
       var name = Recipes.drinks[i].name;
       var tmp = Recipes.drinks[i].ingredients;
 
-      if (tmp.every(r => ingredientArray.includes(r))) {
-        console.log('Found all of', tmp, 'in', ingredientArray);
-        potentialRecipies.push(name)
+      if (tmp.every((r) => ingredientArray.includes(r))) {
+        console.log("Found all of", tmp, "in", ingredientArray);
+        potentialRecipies.push(name);
       } else {
-        console.log('Did not find all of', tmp, 'in', ingredientArray);
+        console.log("Did not find all of", tmp, "in", ingredientArray);
       }
     }
-  }
+  };
 
   return (
     <div className="background">
       <div className="ingredientsTitlePage">What ingredients do you have?</div>
-      <div className="ingredientsInstructions">Add anything and everything that you have in your kitchen! Based on your ingredients, we will generate mysterious recipes for you.</div>
+      <div className="ingredientsInstructions">
+        Add anything and everything that you have in your kitchen! Based on your
+        ingredients, we will generate mysterious recipes for you.
+      </div>
 
       <br></br>
       <br></br>
       <div className="searchBar">
         <SearchBar
-          // value={this.state.value}
-          // onChange={(newValue) => this.setState({ value: newValue })}
-          // onRequestSearch={() => doSomethingWith(this.state.value)}
+        // value={this.state.value}
+        // onChange={(newValue) => this.setState({ value: newValue })}
+        // onRequestSearch={() => doSomethingWith(this.state.value)}
         />
       </div>
       <br></br>
@@ -81,10 +75,7 @@ const AddIngredients = (props) => {
       <br></br>
       <Grid container direction="row" justify="center" alignItems="center">
         <Grid item xs={2}>
-          <div
-            className="foodItemBox"
-            onClick={recipeGenerator}
-          >
+          <div className="foodItemBox" onClick={recipeGenerator}>
             <p>Lemon</p>
             <img className="foodItemImage" src={lemon}></img>
           </div>
@@ -180,6 +171,11 @@ const AddIngredients = (props) => {
           </div>
         </Grid>
       </Grid>
+      <div className="generate">
+        <Button variant="contained" size="large">
+          Generate Recipes
+        </Button>
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,42 +9,43 @@ import "../styles/home.css";
 const Home = (props) => {
   return (
     <React.Fragment>
-    <div class="homeBackground"></div>
-    <div class="home">
+      <div class="homeBackground"></div>
+      <div class="home">
+        <h1>WHO MADE IT?</h1>
+        <h2>What would you like to make today? </h2>
 
-      <h1>WHO MADE IT?</h1>
-      <h2>What would you like to make today? </h2>
-
-      <Grid container direction="row" alignItems="center" justify="center">
-        {/* Card option to select drink */}
-        <Grid item xs={3} className="homeOptionCard homeOptionCardDrink">
-          <img
-            class="homeOptionCardImage homeOptionCardDrink"
-            src={drink}
-          ></img>
-          <br></br>
-          <Link class="homeOptionCardText" to="/add-ingredients">
-            Drink
-          </Link>
+        <Grid container direction="row" alignItems="center" justify="center">
+          {/* Card option to select drink */}
+          <Grid item xs={3} className="homeOptionCard homeOptionCardDrink">
+            <img
+              class="homeOptionCardImage homeOptionCardDrink"
+              src={drink}
+            ></img>
+            <br></br>
+            <Link class="homeOptionCardText" to="/add-ingredients">
+              Drink
+            </Link>
+          </Grid>
+          {/* Forced space between cards */}
+          <Grid item xs={1}></Grid>
+          {/* Card option to select food */}
+          <Grid item xs={3} className="homeOptionCard">
+            <img class="homeOptionCardImage" src={burger}></img>
+            <br></br>
+            <Link class="homeOptionCardText" to="/add-ingredients">
+              Food
+            </Link>
+          </Grid>
         </Grid>
-        {/* Forced space between cards */}
-        <Grid item xs={1}></Grid>
-        {/* Card option to select food */}
-        <Grid item xs={3} className="homeOptionCard">
-          <img class="homeOptionCardImage" src={burger}></img>
-          <br></br>
-          <Link class="homeOptionCardText" to="/add-ingredients">
-            Food
-          </Link>
-        </Grid>
-      </Grid>
-      <br></br><br></br>
-      {/* Instructions so we don't confuse everyone too much */}
-      <p className="instructions">
-        Choose whether you would like to make a delicious Drink or fantastic Food item today.
-        Based on the ingredients you have in your kitchen, you will solve a mystery!
+        <br></br>
+        <br></br>
+        {/* Instructions so we don't confuse everyone too much */}
+        <p className="instructions">
+          Choose whether you would like to make a delicious Drink or fantastic
+          Food item today. Based on the ingredients you have in your kitchen,
+          you will solve a mystery!
         </p>
-    </div>
+      </div>
     </React.Fragment>
   );
 };
