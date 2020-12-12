d5569ead2cd68059962fb94f6be056305438e2df
Naomi <63750118+nvpaul@users.noreply.github.com>
2020-12-12 07:22:13 -0500

    Modifications to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Grid } from "@material-ui/core";
 
+//Import images
 import drink from "../assets/drink.jpg";
 import burger from "../assets/burger.png";
 import background from "../assets/background.png";
+
+//Import styles
 import "../styles/home.css";
+
+//Home page to welcome users and ask them to select whether they are craving a drink or some food
 const Home = (props) => {
   return (
     <div className="home">
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
       <h1>WHO MADE IT?</h1>
       <h2>What would you like to make today? </h2>
-      <br></br>
-      <br></br>
-      <br></br>
+      
       <Grid container direction="row" alignItems="center" justify="center">
+        {/* Card to display drink option */}
         <Grid item xs={3} className="homeOptionCard homeOptionCardDrink">
           <img
             class="homeOptionCardImage homeOptionCardDrink"
@@ -30,7 +29,9 @@ const Home = (props) => {
             Drink
           </Link>
         </Grid>
+        {/* Forced spacing between the card options */}
         <Grid item xs={1}></Grid>
+        {/* Card to display food option */}
         <Grid item xs={3} className="homeOptionCard">
           <img class="homeOptionCardImage" src={burger}></img>
           <br></br>
@@ -39,9 +40,13 @@ const Home = (props) => {
           </Link>
         </Grid>
       </Grid>
-      <br></br><br></br>
-      <p className="instructions">Choose a Drink or Food item that you would like to make today. Based on the ingredients you have, 
-      you will solve a mystery.</p>
+      <br></br>
+      <br></br>
+      {/* Instructions to make sure we don't confuse everyone too much */}
+      <p className="instructions">
+        Choose whether you want to make a delicious Drink or fantastic Food item today.
+        Based on the ingredients you have in your kitchen, you will get the chance to solve a mystery!
+        </p>
     </div>
   );
 };
