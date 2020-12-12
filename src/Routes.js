bc7534d79972a56d97ba8f6700dbaa8c81f55c18
Naomi <63750118+nvpaul@users.noreply.github.com>
2020-12-11 20:11:54 -0500

    added route for recipe steps

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,15 @@
 import AddIngredients from './pages/AddIngredients';
+import RecipeSteps from '.pages/RecipeSteps';
 import Home from './pages/Home';
 
 const Routes = [
   {
     path: '/add-ingredients',
     component: AddIngredients,
+  },
+  {
+    path: '/recipe-steps',
+    component: RecipeSteps,
   },
 ];
 
