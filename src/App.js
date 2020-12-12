e41d2b3fe17021ed3061d4d26bfa61fcdde0718d
Marvi Naaz <marvinaaz@Marvis-MacBook-Air.local>
2020-12-11 19:47:36 -0500

    first page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import drink from "./drink.jpg";
+import burger from "./burger.png";
 
 function App() {
   return (
     <div className="App">
-      <div>Who Made It?</div>
+      <h1>Who made it</h1>
+      <h2>What would you like to make today? </h2>
+      <div className="Card_drink">
+        <img src={drink}></img>
+        <h1>Drink</h1>
+      </div>
+
+      <div className="Card_burger">
+        <img src={burger}></img>
+        <h1>Food</h1>
+      </div>
     </div>
   );
 }
