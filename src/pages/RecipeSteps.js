60cb710baa8ed7245c1910e9d3d71962f3f9a8eb
Naomi <63750118+nvpaul@users.noreply.github.com>
2020-12-11 20:05:32 -0500

    Begun adding text

---
+++ src/pages/RecipeSteps.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+//This page will provide the user with the steps to follow along with the poisoned recipe (minus the poison of coruse)
+//To help them figure out who the killer is, as soon as they find out what they are making
+const RecipeSteps = (props) => {
+  return (
+    <React.Fragment>
+      {/* The Title */}
+      <div>Follow along with the recipe (minus the poison) to solve the mystery and find out who the killer is</div>
+
+      {/* The Recipe Steps */}
+      
+
+      {/* The Suspects */}
+
+      {/* The Menu Options */}
+    </React.Fragment>
+  );
+}
+
+export default RecipeSteps;
