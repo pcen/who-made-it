7f09cae8e74505c3892a723cb2b78dc27a26611d
David S <david.schriemer7@gmail.com>
2020-12-12 13:27:28 -0800

    adding all recipies

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,50 +1,205 @@
 const Recipes = {
-
-  // Drink Recipes
   drinks: {
     // Scotch Tom Collins
     ScotchTomCollins: {
-      ingredients: ['lemon', 'scotch', 'ice', 'soda water'],
+      ingredients: ['lemon', 'whisky', 'ice', 'club soda'],
       steps: [
-        'Fill a cocktail shaker with ice',
-        'Squeeze 5-6 dashes of lemon juice into the shaker',
-        'Add 1oz Scotch',
-        'Shake well!',
-        'Strain into a Collins glass and fill with club soda',
-      ],
-      name: 'Scotch Tom Collins',
+        'combine sugar and 1/3 cup water in a jar',
+        'pour in 1.5 shots of whisky',
+        'add 1/2 cup of club soda',
+        'squeeze in lemon juice'
+      ]
+    },
+    CapeCodder: {
+      ingredients: ['cranberry juice', 'vodka', 'lime', 'ice'],
+      steps: [
+        'add 1 shot of vodka to a glass',
+        'add 1-2 ice cubes',
+        'squeeze in 3/4 ounce lime juice',
+        'add 3 ounces of cranberry juice',
+      ]
+    },
+    Daiquiri: {
+      ingredients: ['rum', 'lime', 'sugar'],
+      steps: [
+        'step 1',
+        'step 2',
+        'step 3',
+      ]
+    },
+    WhiteWineSprizer: {
+      ingredients: ['white wine', 'club soda', 'lemon'],
+      steps: [
+        'add 1/4 cup of club soda to a glass',
+        'add 3/4 cup of white wine to the glass',
+        'garnish with a lemon rind',
+      ]
     },
 
-    // Scotch Rickey
-    ScotchRickey: {
-      ingredients: ['lemon', 'lime', 'ice', 'scotch'],
+    Margarita: {
+      ingredients: ['tequilla', 'lime', 'ice', 'salt'],
       steps: [
-        'Squeeze juice of half a lime and quarter of a lemon into an ice-fillled highball glass',
-        'Pour 1oz scotch',
-        'Fill glass with soda water and stir',
-        'Add lime twist',
-      ],
-      name: 'Scotch Rickey',
+        'run a lime wedge around rim of glass',
+        'dip the rim in a course salt',
+        'pour one shot of tequila into glass',
+        'add 1-2 ice cubes',
+        'add a lime wedge'
+      ]
+    },
+    Cosmopolitan: {
+      ingredients: ['vodka', 'triple sec', 'lime', 'cranberry juice'],
+      steps: [
+        'add 1 shot of vodka',
+        'add ice cubes',
+        'add 1 tablespoon of triple sec',
+        'add 2 tablespoons of cranberry juice',
+        'squeeze in 1/2 ounce of lime juice'
+      ]
+    },
+    MoscowMule: {
+      ingredients: ['lemon', 'scotch', 'ice', 'soda'],
+      steps: [
+        'add 2 shots of vodka to a glass',
+        'add 1/2 ounce of lime to glass',
+        'add 2-3 ice cubes',
+        'stir in 1/2 cup of ginger beer',
+        'drop in lime wedge'
+      ]
+    },
+    ClassicMartini: {
+      ingredients: ['gin', 'vermouth', 'ice', 'lemon'],
+      steps: [
+        'add a 1.5 shots of gin to a glass',
+        'add half a shot of vermouth to the glass',
+        'add cubed ice and stir for 30 seconds',
+        'add a lemon peel to the glass'
+      ]
+    },
+    VodkaMartini: {
+      ingredients: ['vodka', 'vermouth', 'ice', 'lemon'],
+      steps: [
+        'add a 1.5 shots of vodka to a glass',
+        'add half a shot of vermouth to the glass',
+        'add cubed ice and stir for 30 seconds',
+        'add a lemon peel to the glass'
+      ]
+    },
+    Mojito: {
+      ingredients: ['mint leaves', 'rum', 'lime', 'club soda'],
+      steps: [
+        'add 1-2 lime wedges to a glass',
+        'add 2 tablespoons of sugar',
+        'optional step: add 2-3 ice cubes',
+        'add 1 shot of rum',
+        'add 1/2 cup of club soda',
+        'add 5-10 mint leaves'
+      ]
+    },
+    French75: {
+      ingredients: ['gin', 'sugar', 'lemon', 'champagne'],
+      steps: [
+        'add 3/4 ounce of lemon juice to a glass',
+        'add 1.5 shots of gin to the glass',
+        'add 0.5 tablespoon of sugar and 1 tablespoon of water',
+        'add 1.5 shots of champagne'
+      ]
+    },
+    Gimlet: {
+      ingredients: ['gin', 'sugar', 'lime'],
+      steps: [
+        'add 1.5 shots of gin to a glass',
+        'add 0.5 shot of lime juice',
+        'add 0.5 tablespoon of sugar and 1 tablespoon of water',
+      ]
+    },
+    DarkandStormy: {
+      ingredients: ['rum', 'ginger beer'],
+      steps: [
+        'add two shots of ginger beer to a glass',
+        'optional: add 0.5 shots of lime juice to glass',
+        'add 1.5 shots of rum to glass',
+        'add icecubes and stir'
+      ]
+    },
+    MaiTai: {
+      ingredients: ['rum', 'orange juice', 'sugar', 'triple sec'],
+      steps: [
+        'add 2 shots of rum to a glass',
+        'add 0.5 cup of orange juice to the glass',
+        'add 1 shot of triple sec to the glass',
+        'add 0.5 tabespoon of sugar to the glass'
+      ]
+    },
+    Southside: {
+      ingredients: ['gin', 'mint', 'lime'],
+      steps: [
+        'add 1.5 shots of gin to a glass',
+        'add 0.5 shot of lime juice',
+        'add 1-2 mint leaves',
+      ]
+    },
+    Cosmopolitan: {
+      ingredients: ['vodka', 'triple sec', 'cranberry juice', 'lime'],
+      steps: [
+        'add 1 shot of vodka to glass',
+        'add 0.5 shot of lime juice',
+        'add 0.5 shot of triple sec',
+        'add 0.5 shot of cranberry juice'
+      ]
+    },
+    GingerSpritz: {
+      ingredients: ['white wine', 'ginger beer', 'lime'],
+      steps: [
+        'add 3 shots of white win to a glass',
+        'add 2 shots of ginger beer to the glass',
+        'garnish with half a lime',
+      ]
+    },
+    Screwdriver: {
+      ingredients: ['orange juice', 'tequila', 'ice'],
+      steps: [
+        'add 2-3 ice cubes to a glass',
+        'add 1.5 shots of tequila to a glass',
+        'add 4 shots of orange juice to the glass',
+      ]
+    },
+    OrangeBlossom: {
+      ingredients: ['orange juice', 'gin', 'vermouth'],
+      steps: [
+        'add 1 shot of gin to a glass',
+        'add 1 shot of vermouth',
+        'add 1 shot of orange juice',
+      ]
+    },
+    BishopCocktail: {
+      ingredients: ['rum', 'red wine', 'sugar', 'lime'],
+      steps: [
+        'add 0.5 tablespoon of sugar and 1 tablespoon of water to a glass',
+        'add 2 shots of rum to the glass',
+        'add 1 shot of red wine',
+        'squeeze in half a lime'
+      ]
+    },
+    RedWineSpritzer: {
+      ingredients: ['red wine', 'club soda', 'ice', 'mint leaves'],
+      steps: [
+        'fill glass 1/2 full of ice',
+        'pour wine over ice till 1/2 full',
+        'fill remaining glass with club soda',
+        'garnish with mint leaves'
+      ]
+    },
+    Screwdriver: {
+      ingredients: ['orange juice', 'tequila', 'ice'],
+      steps: [
+        'add 2-3 ice cubes to a glass',
+        'add 1.5 shots of tequila to a glass',
+        'add 4 shots of orange juice to the glass',
+      ]
     },
 
-    // Whiskey Sour
-    WhiskeySour: {
-      ingredients: ['lemon', 'scotch', 'ice', 'soda water', 'sugar'],
-      steps: [
-        'Fill a cocktail shaker with ice',
-        'Pour scotch, juice of half a lemon and half teaspoon of sugar into the shaker',
-        'Shake well!',
-        'Strain into old-fashioned glass filled with fresh ice',
-        'Serve with squirt of soda water',
-      ],
-      name: 'Whiskey Sour',
-    },
-  },
 
-  // Food Recipes
-  food: {
+    }
 
-  },
+
 };
-
-export default Recipes;
