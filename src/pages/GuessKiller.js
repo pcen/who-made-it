171046b2c53e3fd3280ed3d456aa8cc095195317
Paul Cento <prscento@uwaterloo.ca>
2020-12-11 23:58:57 -0500

    add selection logic

--- src/pages/GuessKiller.js
+++ src/pages/GuessKiller.js
@@ -1,63 +1,58 @@
-import React from 'react';
-import { Grid, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 import '../styles/guesskiller.css';
 import beth from '../assets/beth.png';
 import marge from '../assets/marge.png';
 import ray from '../assets/ray.png';
 
-const GuessKiller = (props) => {
-    return (
-      <React.Fragment>
-        <div className="white-background">
-          <br></br>
-          <br></br>
-          <div className="title">Select the Killer</div>
-          <br></br>
-          <br></br>
-          <div className="subtitle">Be careful because you only get one try!</div>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-            <Grid item xs={3}>
-                <div className="itemBox">
-                <img className="itemImage" src={beth}></img>
-                <p class="personName">Elizabeth</p>
-                <p class="foodName">Scotch Tom Collins</p>
-                </div>
-            </Grid>
-            <Grid item xs={3}>
-                <div className="itemBox">
-                <img className="itemImage" src={marge}></img>
-                <p class="personName">Margaret</p>
-                <p class="foodName">Scotch Rickey</p>
-                </div>
-            </Grid>
-            <Grid item xs={3}>
-                <div className="itemBox">
-                <img className="itemImage" src={ray}></img>
-                <p class="personName">Raymond</p>
-                <p class="foodName">Whiskey Sour</p>
-                </div>
-            </Grid>
-        </Grid>
-        <Button variant="contained" className="buttonBlack">
-            Go Back
-        </Button>
-        <Button variant="contained" className="buttonRed">
-            Submit
-        </Button>
+const KillerProfile = props => {
+  const { image, name, item, onSelect, selected } = props;
 
+  const handleClick = (event) => {
+    onSelect(name);
+  };
+
+  return (
+    <div className='killer-profile'>
+      <img className="itemImage" src={image}></img>
+      {
+        selected === name ?
+        <div class="personNameSelected">{name}</div> :
+        <div onClick={handleClick} class="personName">{name}</div>
+      }
+      <div class="foodName">{item}</div>
+    </div>
+  );
+}
+
+const GuessKiller = props => {
+  const [guess, setGuess] = useState('');
+
+  console.log('guess: ' + guess);
+
+  return (
+      <div className="guess-killer-container">
+        <div>
+          <div className="title">Select the Killer</div>
+          <div className="subtitle">Be careful because you only get one try!</div>
         </div>
-    </React.Fragment>
+
+        <div className='killer-profiles'>
+          <KillerProfile image={beth} name='Elizabeth' item='Scotch Tom Collins' onSelect={setGuess} selected={guess}/>
+          <KillerProfile image={marge} name='Margaret' item='Scotch Rickey' onSelect={setGuess} selected={guess}/>
+          <KillerProfile image={ray} name='Raymond' item='Whiskey Sour' onSelect={setGuess} selected={guess}/>
+        </div>
+
+        <div className='guess-killer-buttons'>
+          <Button variant="contained" className="buttonBlack">
+            Go Back
+          </Button>
+          <Button variant="contained" className="buttonRed">
+            Submit
+          </Button>
+        </div>
+      </div>
   );
 }
 
 export default GuessKiller;
-    
