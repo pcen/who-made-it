5d94e3092af8214f141897a198189f9c2bb28e99
Paul Cento <prscento@uwaterloo.ca>
2020-12-12 02:51:55 -0500

    story links to recipe steps

--- src/pages/TheStory.js
+++ src/pages/TheStory.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import { generateStory } from '../logic/story-generation';
-import Recipes from '../Components/Recipes';
+import { useHistory } from 'react-router-dom';
 
 import Elizabeth from '../assets/beth.png';
 import Margaret from '../assets/marge.png';
 import Raymond from '../assets/ray.png';
 
 import '../styles/the-story.css';
+import '../styles/buttons.css';
 
 const ActorProfile = props => {
   const { image, name, item } = props;
@@ -21,14 +23,23 @@ const ActorProfile = props => {
 
 const TheStory = (props) => {
   const { recipe, suspects } = props;
-  let story = generateStory(Recipes.drinks.ScotchTomCollins, ['Scotch Tom Collins', 'Scotch Rickey', 'Whiskey Sour']);
+  const history = useHistory();
+
+  let story = generateStory(recipe, ['Scotch Tom Collins', 'Scotch Rickey', 'Whiskey Sour']);
+
+  const toSteps = () => {
+    history.push('/recipe-steps');
+  };
 
   return (
     <div className='story-container'>
       <div className="storyTitle">TITLE PLACEHOLDER</div>
       <br></br><br></br>
       <div className='story'>{story}</div>
-      <br></br>
+      <Button variant='contained' className='buttonBlack' onClick={toSteps}>
+        Begin!
+      </Button>
+
       <div className='actors-container'>
         <ActorProfile name='Elizabeth' image={Elizabeth} item={suspects.Elizabeth.recipe} />
         <ActorProfile name='Margaret' image={Margaret} item={suspects.Margaret.recipe} />
