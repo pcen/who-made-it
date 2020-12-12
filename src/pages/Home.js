eb4623c275cafed51f51da5615e746d251f6c62a
Marvi Naaz <marvinaaz@Marvis-MacBook-Air.local>
2020-12-12 00:50:08 -0500

    remove animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { Grid } from "@material-ui/core";
 
 import drink from "../assets/drink.jpg";
@@ -9,35 +9,27 @@ import "../styles/home.css";
 const Home = (props) => {
   return (
     <div className="home">
+      <h1>Who made it</h1>
+      <h2>What would you like to make today? </h2>
+      <br></br>
+      <br></br>
+      <br></br>
       <Grid container direction="row" alignItems="center" justify="center">
-        <div class="area">
-          <ul class="circles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <h1>Who made it</h1>
-        <br></br>
-        <h2>What would you like to make today? </h2>
         <Grid item xs={3} className="homeOptionCard homeOptionCardDrink">
           <img
             class="homeOptionCardImage homeOptionCardDrink"
             src={drink}
           ></img>
-          <Link class="homeOptionCardText" to='/add-ingredients'>Drink</Link>
+          <Link class="homeOptionCardText" to="/add-ingredients">
+            Drink
+          </Link>
         </Grid>
         <Grid item xs={1}></Grid>
         <Grid item xs={3} className="homeOptionCard">
           <img class="homeOptionCardImage" src={burger}></img>
-          <Link class="homeOptionCardText" to='/add-ingredients'>Food</Link>
+          <Link class="homeOptionCardText" to="/add-ingredients">
+            Food
+          </Link>
         </Grid>
       </Grid>
     </div>
