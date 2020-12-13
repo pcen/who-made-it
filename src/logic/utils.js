faf72a5b655f7a1bf8c673e8e33f81a53a9a1989
Paul Cento <prscento@uwaterloo.ca>
2020-12-12 20:12:36 -0500

    commenting

--- src/logic/utils.js
+++ src/logic/utils.js
@@ -1,24 +1,33 @@
+// Returns the integer URL parameter named 'name'
+// - pass useLocation as 'location' parameter
 const getURLInt = (name, location) => {
   return parseInt(new URLSearchParams(location.search).get(name));
 }
 
+// Returns the string URL parameter named 'name'
+// - pass useLocation as 'location' parameter
 const getURLStr = (name, location) => {
   return new URLSearchParams(location.search).get(name);
 }
 
+// Returns true if every item in the array is in the set
 const arrayInSet = (array, set) => {
   return array.every((e) => { return set.has(e); })
 }
 
+// Return a random integer between the given min and max
 const randInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const randFrom = list => {
-  let index = randInt(0, list.length - 1);
-  return list[index];
+// Returns a random element from the given array
+const randFrom = array => {
+  let index = randInt(0, array.length - 1);
+  return array[index];
 }
 
+// Returns the contents of an array as a comma separated list
+// the final element is additionally separated by 'and'
 const commaAndAndSeparate = a => {
   return a.join(', ').replace(/,(?!.*,)/gmi, ', and');
 }
--- src/pages/Finit.js
+++ src/pages/Finit.js
@@ -7,6 +7,8 @@ import beth from '../assets/beth.png';
 import marge from '../assets/marge.png';
 import ray from '../assets/ray.png';
 
+// Returns who the killer was based off of the recipe that was assigned
+// to the user and the recipe that each of the suspects made
 const getKiller = (recipe, suspects) => {
   let killer = null;
   Object.keys(suspects).forEach(name => {
@@ -15,12 +17,14 @@ const getKiller = (recipe, suspects) => {
   return killer;
 }
 
+// Returns the name of the recipe that was used to poison the victim
 const getFatalRecipe = (killer, suspects) => {
   console.log(killer);
   console.log(suspects);
   return suspects[killer].recipe;
 }
 
+// Component displaying the killer's name, recipe, and image
 const KillerProfile = props => {
   const { name, recipe } = props;
   const images = { 'Elizabeth': beth, 'Margaret': marge, 'Raymond': ray };
@@ -36,30 +40,36 @@ const KillerProfile = props => {
   )
 }
 
+// Final page component upon completing the recipe
+// Reveals if the user's guess was correct or not and describes the
+// aftermath of the murder
 const Finit = props => {
   const { recipe, suspects, guess, setRecipe, setRecipeOptions, setSuspects, setGuess } = props;
   const history = useHistory();
 
+  // Determine if the user's guess was correct
   let correct = false;
   if (Object.keys(guess).length !== 0) {
     correct = suspects[guess.suspect].recipe === recipe.name;
   }
 
+  // Get the killer and the recipe they used when poisoning the victim
   const killer = getKiller(recipe, suspects);
   const fatalRecipe = getFatalRecipe(killer, suspects);
 
+  // Conditional text based off of whether or not the user's guess was correct
   const title = correct ?
     'Great job! You\'d make a good detective!'
     : `So close! The killer was ${killer}`;
   const subtitle = correct ?
     `The killer was ${killer}`
     : `${killer} got away with the murder`;
-
   const conclusion = correct ?
     `${killer} skipped town, thinking they had escaped. Luckily, and keen detective such as
     yourself rose to the challenge and solved the mystery!`
     : `${killer} skipped town and escaped!`;
 
+  // Resets the game state and returns the user to the home page
   const playAgain = () => {
     setRecipe({});
     setRecipeOptions([]);
@@ -69,30 +79,29 @@ const Finit = props => {
   }
 
   return (
-    <React.Fragment>
       <div className="redBackground">
         <div className="white-card">
+          {/* Title */}
           <div className="title">{title}</div>
           <br></br>
           <div className="subtitle">{subtitle}</div>
           <br></br>
           <br></br>
-
+          {/* The Killer's Profile */}
           <KillerProfile name={killer} recipe={fatalRecipe} /><br></br>
-
+          {/* Concluding Story */}
           <div className="storyConclusion">{`The victim once had a secret affair with none other than
             ${killer}. After a brutal separation, ${killer} was quick to get their revenge. They added a drop of poison
             to their ${fatalRecipe} and served it to the victim on the night of the murder. ${conclusion}
             Click play again to solve another mystery.${correct ? '' : ' Better luck next time!'}`}</div>
-
+          {/* Play Again */}
           <Button variant="contained" className="buttonRedLong"
             onClick={playAgain}
           >
             Play Again
-            </Button>
+          </Button>
         </div>
       </div>
-    </React.Fragment>
   );
 }
 
