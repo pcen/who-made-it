97221fdc0df1ed9d40e00a8f778a80f82bb789f3
Marvi Naaz <marvinaaz@Marvis-MacBook-Air.local>
2020-12-12 20:18:02 -0500

    small changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 
- /*Link to add ingredients page*/
+/*Link to add ingredients page*/
 import { Link } from "react-router-dom";
 
 import { Grid } from "@material-ui/core";
 import drink from "../assets/drink.jpg";
 import "../styles/home.css";
 
+//This is the main function for the home page
 const Home = (props) => {
   return (
     <React.Fragment>
@@ -14,7 +15,8 @@ const Home = (props) => {
 
       {/* Game Title */}
       <div class="home">
-        <br></br><br></br>
+        <br></br>
+        <br></br>
         <h1>WHO MIXED IT?</h1>
         <br></br>
 
@@ -24,7 +26,8 @@ const Home = (props) => {
           <Grid item xs={3} className="homeOptionCard homeOptionCardDrink">
             <img
               class="homeOptionCardImage homeOptionCardDrink"
-              src={drink}></img>
+              src={drink}
+            ></img>
             <br></br>
 
             {/* Link to start the game - add ingredients page */}
@@ -39,9 +42,10 @@ const Home = (props) => {
 
         {/* Brief Game Instructions*/}
         <p className="instructions">
-          In Who Mixed It, you select drink ingredients that you have in your kitchen.
-          Based on these ingredients, you will follow an unknown recipe to create a delicious drink.
-          Figuring out what drink you are mixing will help you solve a murder mystery.
+          In Who Mixed It, you select drink ingredients that you have in your
+          kitchen. Based on these ingredients, you will follow an unknown recipe
+          to create a delicious drink. Figuring out what drink you are mixing
+          will help you solve a murder mystery.
         </p>
       </div>
     </React.Fragment>
